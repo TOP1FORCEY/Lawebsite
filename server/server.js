3c8ec15d54555b409cb1f2bc63a88e2425be6af4
Eugene Rudenko <romajen2114@gmail.com>
2025-01-23 00:49:35 +0200

    test

--- server/server.js
+++ server/server.js
@@ -12,10 +12,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
